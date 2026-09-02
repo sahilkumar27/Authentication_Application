79c3d4ab8aa111f607c43e2046323831c6d0d584
sahilkumar27 <sahildhn27@gmail.com>
2026-09-03 01:02:25 +0530

    updated till lecture 6

---
+++ src/main/java/com/substring/auth/auth_app_backend/services/UserService.java
@@ -0,0 +1,26 @@
+package com.substring.auth.auth_app_backend.services;
+
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+
+public interface UserService {
+
+    //create user
+    UserDto createUser(UserDto userDto);
+
+    //get user by email
+    UserDto getUserByEmail(String email);
+
+    //update user
+    UserDto updateUser(UserDto userDto, String userId);
+
+    //delete user
+    void deleteUser(String userId);
+
+    //get user by id
+    UserDto getUserByUsername(String username);
+
+    //get all users
+    Iterable<UserDto> getAllUsers();
+
+
+}
---
+++ src/main/java/com/substring/auth/auth_app_backend/services/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.substring.auth.auth_app_backend.services;
+
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, String userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public UserDto getUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Iterable<UserDto> getAllUsers() {
+        return null;
+    }
+}
